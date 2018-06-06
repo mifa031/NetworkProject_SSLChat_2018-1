37bf73d9dd30e0dc6724cc4348c263327bdc21c6
MrJang <whdgus8809@naver.com>
2018-06-06 18:17:49 +0900

    Add files via upload

---
+++ InstantSSLMessenger/MessengerClient.java
@@ -0,0 +1,6 @@
+public class MessengerClient {
+	public static void main(String args[]) throws Exception {
+		//서버쪽으로 연결 
+		
+	}
+}
---
+++ InstantSSLMessenger/MessengerClientReceiver.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.nio.channels.SocketChannel;
+
+import javax.net.ssl.SSLEngine;
+import javax.net.ssl.SSLEngineResult;
+//리시버 함수 O 센더 함수 x
+public class MessengerClientReceiver implements Runnable {
+	public void run() {
+		
+	}
+}
