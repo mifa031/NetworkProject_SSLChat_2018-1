c5a6c420c83cc447e50ab306bb36bfd6e63111c8
mifa031 <scifimov@naver.com>
2018-06-19 01:45:57 +0900

    Add files via upload

---
+++ MessengerClientReceivedMsg.java
@@ -0,0 +1,4 @@
+
+public class MessengerClientReceivedMsg {
+	public String msg;
+}
---
+++ MessengerRoomUserInfo.java
@@ -0,0 +1,24 @@
+import java.nio.channels.SelectionKey;
+import java.util.HashMap;
+import java.util.Vector;
+
+import javax.net.ssl.SSLEngine;
+
+class UserInfo{
+	public String id;
+	public SSLEngine engine;
+	public SelectionKey key;
+	public String roomname;
+}
+
+public class MessengerRoomUserInfo {
+	public Vector<String> room;
+	public Vector<UserInfo> info; 
+	public HashMap<String,Vector<UserInfo>> map = new HashMap<String,Vector<UserInfo>>(); 
+	
+	public MessengerRoomUserInfo(){
+		room = new Vector<String>();
+		info = new Vector<UserInfo>();
+		map  = new HashMap<String,Vector<UserInfo>>();		
+	}
+}
